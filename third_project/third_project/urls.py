9a559f4ecdd856b047cb4dc61b6816a0cf7010b2
maruf-rahman007 <s.marufrahman.g@gmail.com>
2023-10-03 01:43:05 +0600

    Boostrap Added and URL Tag

--- third_project/third_project/urls.py
+++ third_project/third_project/urls.py
@@ -22,7 +22,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home,name="homepage"),
+    path('about/',views.about,name="aboutpage"),
     path('first_app/',include('first_app.urls')),
-    + static(settings.MEDIA_DIR,document_root = settings.MEDIA_ROOT)
-]
+    
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- third_project/third_project/views.py
+++ third_project/third_project/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
+
 def home(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
