ba52b0f0be9fae0310bfbe4942590ef92414a803
maruf-rahman007 <s.marufrahman.g@gmail.com>
2023-11-05 02:14:50 +0600

    More works with forms/buildin forms in django/ password matching

--- fifth_project/first_app/urls.py
+++ fifth_project/first_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path,include
+from django.urls import path
 from . import views
-
 urlpatterns = [
-    
-    path('',views.home,name = "homepage"),
-    path('about/',views.about,name = "aboutpage"),
-    path('form/',views.form,name = "submit_form"),
-    path('django_form/',views.DjangoForm,name = "django_form")
+    path('', views.home, name = "homepage"),
+    path('about/', views.about, name = "aboutpage"),
+    path('form/', views.submit_form, name = "submit_form"),
+    path('django_form/', views.PasswordValidation, name = "django_form"),
 ]
